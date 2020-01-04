51851edbb377bf7c7c3d28829fa2f46bd5f3f228
Jyoti <jyoti.contact@gmail.com>
2020-01-04 21:05:33 +0000

    add Avatar react component

--- reactPropsUdemy/src/components/Card.js
+++ reactPropsUdemy/src/components/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Avatar from './Avatar';
 
 function Card(props) {
   return (
@@ -6,7 +7,7 @@ function Card(props) {
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img className="circle-img" src={props.img} alt="avatar" />
+          <Avatar img={props.img} />
         </div>
         <div className="bottom">
           <p className="info">{props.tel}</p>
